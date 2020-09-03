80816178c05f5d0ea261ce82de91bb4e36d847d9
Gobind Puniani <gobindpuniani@gmail.com>
2020-09-03 11:57:52 -0700

    Added print and input statements for user

---
+++ rocket_propellant_ratio_calculator.py
@@ -0,0 +1,11 @@
+
+
+#These two print statements orient the user as to the purpose and proper usage of this calculator
+print("Welcome! This calculator uses the Tsiolkovsky rocket equation (also called the 'ideal rocket equation') to determine the proportion",
+"of the mass of propellant (fuel) relative to the weight of the full rocket (including propellant, tank, engines, payload, etc.).")
+print("This calculator will require two input values. One is 'delta-v', which is the total change in velocity.",
+"For example, a rocket starting from Earth's surface and ending in Low Earth Orbit could have a 'delta-v'",
+"of 9.7 km/s, which is the final speed of the rocket on this path. The other input value is the effective",
+"exhaust velocity, which is the velocity of the material ejected from the bottom of the rocket to produce thrust.",
+"Continuing with our previous scenario of a rocket bound for Low Earth Orbit, the effective exhaust velocity",
+"might be 4.5 km/s. More information about this rocket equation can be found in the resources below.")
